d62b1ed039c0ecfd09e89d861f677998b2eaeb2e
Ben Cöppicus <ben.coeppicus@it-scholar.com>
2026-06-23 14:37:56 +0200

    add azurerm remote backend for group-infra (stterraformlab24)

---
+++ examples/group-infra/providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.5.0"
+
+  backend "azurerm" {
+    resource_group_name  = "rg-tf-lab"
+    storage_account_name = "stterraformlab24"
+    container_name       = "tfstate"
+    key                  = "group-infra.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
